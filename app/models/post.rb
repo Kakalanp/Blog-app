4ced75329411be2193539b7d0dab363532bc0d07
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-07 16:54:08 -0500

    Adding methods to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def update_post_counter
+    user.increment!(:PostsCounter)
+  end
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
