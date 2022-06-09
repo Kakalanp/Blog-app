16ee481c2c3a03bc2d4498a73a8fcc65624036c2
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-09 15:23:13 -0500

    Adding method new on post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,21 @@ class PostsController < ApplicationController
     @user = User.find(params['user_id'])
     @post = Post.find(params['id'])
   end
+
+  def new
+    @user = User.find(params['user_id'])
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :user_id)
+  end
 end
