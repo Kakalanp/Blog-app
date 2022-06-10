2b0cb3e4e198b14c5867bb6a81f7030dff65ee76
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-10 16:57:30 -0500

    Adding user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :nullify
-  validates :name, presence: true
+  
+  validates :Name, presence: true
+  validates :PostsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
