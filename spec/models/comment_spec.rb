7c4cccd552dc990c05730d619638c7e8ffa4d384
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-10 20:06:00 -0500

    Adding comment specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:example) { @comment = Comment.new(text: 'test', post_id: 1, user_id: 1) }
+
+  describe 'validation' do
+    it 'correctly validates fields' do
+      @comment.save
+      expect(@comment).to be_valid
+
+      @comment = Comment.new(text: 'no post', user_id: 1)
+      @comment.save
+      expect(@comment).to_not be_valid
+    end
+  end
+
+  describe 'methods' do
+    it 'correctly uses update_comments_counter' do
+      @comment.save
+      @comment.update_comments_counter
+      expect(comment.post.commentsCounter).to eql(0)
+    end
+  end
 end
