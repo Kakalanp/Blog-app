c3fca95fecb4077a6c29adffed1e237ff87acbbe
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-11 07:54:03 -0500

    initialized postsCounter at 0 on creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,15 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :postsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  after_save :initialize_post_counter
+
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  private
+
+  def initialize_post_counter
+    self.postsCounter = 0
   end
 end
