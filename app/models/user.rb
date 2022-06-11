2b74c8d8d31d3550bdba73f79afd753053c34614
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-11 07:33:46 -0500

    Fixing typo in user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :nullify
 
-  validates :Name, presence: true
-  validates :PostsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :postsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
