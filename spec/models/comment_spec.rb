cfe3f0a147d8cca8ab129bb5f40fd6abc6244ddf
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-15 19:02:01 -0500

    Fixing comment model tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'comment', type: :model do
-  before(:example) { @comment = Comment.new(text: 'test', post_id: 1, user_id: 1) }
-
   describe 'validation' do
     it 'correctly validates fields' do
-      @comment.save
-      expect(@comment).to be_valid
+      comment = Comment.new(text: 'testing commnent', post_id: 1, user_id: 1)
+      comment.save
+      expect(comment).to be_valid
 
-      @comment = Comment.new(text: 'no post', user_id: 1)
-      @comment.save
-      expect(@comment).to_not be_valid
+      comment = Comment.new(text: 'testing commnent', user_id: 1)
+      comment.save
+      expect(comment).to_not be_valid
     end
   end
 
   describe 'methods' do
     it 'correctly uses update_comments_counter' do
-      @comment.save
-      @comment.update_comments_counter
-      expect(@comment.post.commentsCounter).to eql(1)
+      comment = Comment.new(text: 'testing commnent', post_id: 1, user_id: 1)
+      comment.save
+      comment.send(:update_comments_counter)
+      expect(comment.post.commentsCounter).to eql(3)
     end
   end
 end
